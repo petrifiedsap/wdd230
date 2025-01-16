36153a12ca7c320d52706b8ee3f91b3675b70aea
petrifiedsap <amberboyd@byui.edu>
2025-01-16 01:03:06 -0600

    fixed JS and finalized style

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,9 @@
-getfullyear();
+// current year
+const todaysdate = new Date();
+const year = todaysdate.getFullYear();
+document.getElementById('current-year').innerHTML = ('&copy') + year;
+
+
+
+// last modified date
+   document.getElementById('lastmodified').innerHTML = document.lastModified;
