5918f9ef6b9322e7e42a4bf07d264d24d4ae9cff
petrifiedsap <amberboyd@byui.edu>
2025-01-22 22:38:24 -0600

    added map, pseudo element, fixed hamburger menu padding

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,3 +7,12 @@ document.getElementById('current-year').innerHTML = ('&copy') + year;
 
 // last modified date
    document.getElementById('lastmodified').innerHTML = document.lastModified;
+
+// for hamburger menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
