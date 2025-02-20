8025d96717e2c3a375792ba863d166243cf3968e
petrifiedsap <amberboyd@byui.edu>
2025-02-19 21:53:13 -0600

    page visits counter

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -16,3 +16,19 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = 'Welcome';
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
+
+});
+//window.addEventListener('load', function() 
