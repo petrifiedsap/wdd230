56771e273e417e9794ef2e7265162869a2460e08
petrifiedsap <amberboyd@byui.edu>
2025-02-25 18:08:55 -0600

    got the formatting better and the hover effect on the pictures

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -10,7 +10,6 @@ function windChill(){
 if (temp <= 50 && wind > 3.0) {
     let windchill;
     windchill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
-    console.log(windchill);
 
     chill.innerHTML = windchill.toFixed(0);
 }
