9442383a21992241468619a78867b8160e3a444d
petrifiedsap <amberboyd@byui.edu>
2025-03-06 22:59:23 -0600

    ward list

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,11 @@ const linksURL = "https://petrifiedsap.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
 }
 
 getLinks();
+
+function displayLinks(weeks) {
+    
+}
