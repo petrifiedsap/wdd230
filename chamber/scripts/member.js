a08e29a341f330019bd60ebc26b20f35e7dc9a1e
petrifiedsap <amberboyd@byui.edu>
2025-03-08 20:19:36 -0600

    standardized pics and did layout and toggle

--- chamber/scripts/member.js
+++ chamber/scripts/member.js
@@ -14,7 +14,7 @@ const displayMembers = (members) => {
     members.forEach((member) => {
         let card = document.createElement('section');
         let logo = document.createElement('img');
-        let name = document.createElement('h2');
+        let name = document.createElement('h3');
         let address = document.createElement('p');
         let phone = document.createElement('p');
         let website = document.createElement('a');
@@ -26,7 +26,8 @@ const displayMembers = (members) => {
         logo.setAttribute('height', "auto");
         name.textContent = `${member.name}`;
         address.textContent = `${member.address}`;
-        phone.textContent = 'member.phoneNumber';
+        phone.setAttribute("class", "phone");
+        phone.textContent = `${member.phoneNumber}`;
         website.setAttribute('href', member.websiteURL);
              
         card.appendChild(logo);
@@ -37,6 +38,24 @@ const displayMembers = (members) => {
 
         cards.appendChild(card);
     });
+}
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#member-cards");
 
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", showGrid);
+
+function showGrid() {
+  display.classList.add("grid");
+  display.classList.remove("list");
+}
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+  display.classList.add("list");
+  display.classList.remove("grid");
 }
