7eb2afda5b125b6fd46a7b034928caa663ac7464
petrifiedsap <amberboyd@byui.edu>
2025-03-13 20:12:40 -0500

    weather in home page

---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#description');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=33.02&lon=-96.70&appid=9a3623d99a7e974b0da966d7fba6576e&units=imperial'
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description; 
+    const capsDesc = desc.replace(/(^\w|\s\w)/g, m => m.toUpperCase());
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', "icon of weather");
+    captionDesc.textContent = `${capsDesc}`;
+}
+
