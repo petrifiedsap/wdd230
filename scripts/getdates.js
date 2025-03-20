8ba7e4a5d6f3af83799fad3b8395553fe14856f5
petrifiedsap <amberboyd@byui.edu>
2025-03-19 23:19:17 -0500

    fixed form page formulas

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,7 +4,6 @@ const year = todaysdate.getFullYear();
 document.getElementById('current-year').innerHTML = ('&copy') + year;
 
 
-
 // last modified date
    document.getElementById('lastmodified').innerHTML = document.lastModified;
 
@@ -17,6 +16,7 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
+// counter to show number of visits
 document.addEventListener('DOMContentLoaded', function() {
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
@@ -37,9 +37,9 @@ localStorage.setItem("numVisits-ls", numVisits);
 {
 const pw1 = document.querySelector("#password");
 const pw2 = document.querySelector("#password2");
-if (pw2 != null) {	
 
-const message = document.querySelector("#form-message");
+if (pw2 !== null) {	
+	const message = document.querySelector("#formmessage");
 // add function to check if pw2 exists, if null return
 pw2.addEventListener("focusout", checkSame);
 
@@ -47,13 +47,13 @@ function checkSame() {
 	if (pw1.value !== pw2.value) {
 	
 		message.textContent = "❌ Passwords do not match!";
-		message.style.visibility = "show";
+		message.style.visibility = "visible";
 		//put other styles here
 		pw1.value = "";
 		pw2.value = "";
 		pw1.focus(); 
 	} else {
-		message.style.display = "none";
+		message.style.visibility = "hidden";
 	}
 }
 }
@@ -62,19 +62,19 @@ function checkSame() {
 {
 const email = document.querySelector("#email");
 const emailPattern = /[@]byui\.edu/;
-const message = document.querySelector("#form-message");
+const message = document.querySelector("#formmessage");
 
-if (email != null) { 
+if (email !== null) { 
 email.addEventListener("focusout", checkMail);
 
 function checkMail() {
-	if (emailPattern.test(email.value) = '') {
+	if (!emailPattern.test(email.value)) {
 		message.textContent = "❌ Please enter a valid byui.edu email address";
-		message.style.visibility = "show";
+		message.style.visibility = "visible";
 		email.value = "";
 		email.focus();
 	} else {
-		message.style.display = "none";
+		message.style.visibility = "hidden";
 	}
 }
 }
