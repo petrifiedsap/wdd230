2504d283d289c07323d1e0b6635a66f39188d181
petrifiedsap <amberboyd@byui.edu>
2025-03-20 00:32:29 -0500

    fixed link display on index

--- scripts/links.js
+++ scripts/links.js
@@ -1,14 +1,30 @@
 const baseURL = "https://petrifiedsap.github.io/wdd230/";
 const linksURL = "https://petrifiedsap.github.io/wdd230/data/links.json";
+const lessonList = document.querySelector('#datadisplay');
+
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    console.table(data);
+    displayLinks(data.lessons);
 }
 
 getLinks();
 
-function displayLinks(weeks) {
-    
+
+function displayLinks(lessons){
+    lessons.forEach(lesson => {
+        const lessonItem = document.createElement('li');
+        lessonItem.textContent = `${lesson.lesson}:  `;
+
+        lesson.links.forEach(link => {
+            const linkItem = document.createElement('a');
+            linkItem.href = link.url;
+            linkItem.textContent = `${link.title} `;
+            lessonItem.appendChild(linkItem);
+        });
+
+        lessonList.appendChild(lessonItem);
+    });
 }
